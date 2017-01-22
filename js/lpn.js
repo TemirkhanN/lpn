0b20a334b9d1ca52bcfa7f33b50be1dffeb57410
Temirkhan <diezza.com@gmail.com>
2017-01-22 17:39:00 +0300

    It was sad holidays. So I tried some refactoring

--- js/lpn.js
+++ js/lpn.js
@@ -1,15 +1,19 @@
 /**
  *
  * @param message text that will be shown in popup. Popup won't be shown if empty or null value passed
- * @param messageType color of popup will be generated based on type. Default: 'notice'
- * Success is green popup. Notice is blue. Warning is yellow. Error is red.
+ * @param messageType color of popup will be generated based on type.
+ * Success is green popup; Notice is blue; Warning is yellow; Error is red;
+ * Default: notice;
  *
  * @param position html based position of popup. Default: 'bottom left'
  * @param duration time in seconds before popup disappear. default is 5 seconds
  */
-
-function popupMessage(message, messageType, position, duration) {
-
+function PopupMessage(message, messageType, position, duration) {
+    /**
+     * Available message types
+     *
+     * @type {*}
+     */
     var messageTypes = {
         success: 'success',
         notice: 'notice',
@@ -17,100 +21,189 @@ function popupMessage(message, messageType, position, duration) {
         error: 'error'
     };
 
-    duration = typeof duration == 'undefined' ? 5 : parseInt(duration);
-
-    var popup = document.createElement('div');
-    var closePopup = document.createElement('span');
-    var popupClass = 'lpn-window';
-
-    messageType = typeof messageType == 'undefined' || ! messageType.toLowerCase() in messageTypes ? messageTypes.notice : messageType.toLowerCase();
-    message = typeof message == 'undefined' || message.length == 0 ? null : message;
-    position = typeof position == 'undefined' || position.length == 0 ? 'bottom left' : position;
-
-    this.popupId = typeof this.popupId == 'undefined' ? 1 : this.popupId + 1;
-    var _this = this;
-
+    /**
+     * Elements DOM parameters such as style properties, class name and etc.
+     *
+     * @type {*}
+     */
+    var elements = {
+        popupWindow: {
+            className: 'lpn-window',
+            style: {
+                fontSize: '14px',
+                position: 'fixed',
+                zIndex: '100000',
+                padding: '20px',
+                marginBottom: '20px;',
+                border: '1px solid transparent',
+                borderRadius: '4px'
+            }
+        },
+        hideButton: {
+            className: 'close-popup',
+            innerHTML: '×',
+            title: 'Hide',
+            style: {
+                position: 'absolute',
+                top: '0',
+                right: '5px',
+                fontSize: '18px',
+                fontWeight: 'bold',
+                cursor: 'pointer'
+            }
+        }
+    };
 
     /**
-     * Generates popup and output it to browser
+     * Constructor
      */
-    var generatePopup = function () {
-        if (!validPopupDeclaration()) {
-            return;
-        }
+    var popupMessage = function (message, type, position, duration) {
+        var popupAttributes       = elements.popupWindow;
+        popupAttributes.innerHTML = message;
 
-        setPosition();
-        popup.id = getUniqueId();popup.className = popupClass;popup.style.fontSize = '14px';popup.style.position = 'fixed';popup.style.zIndex = '100000';popup.style.padding = '20px';popup.style.marginBottom = '20px;';popup.style.border = '1px solid transparent';popup.style.borderRadius = '4px';
-        popup.innerHTML = message;
+        var popup = fillAttributes(document.createElement('div'), popupAttributes);
 
-        switch (messageType) {
-            case messageTypes.success:
-                popup.style.color = '#3C763D';popup.style.backgroundColor = '#DFF0D8';popup.style.borderColor = '#D6E9C6';
-                break;
-            case messageTypes.warning:
-                popup.style.color = '#8A6D3B';popup.style.backgroundColor = '#FCF8E3';popup.style.borderColor = '#FAEBCC';
-                break;
-            case messageTypes.error:
-                popup.style.color = '#A94442';popup.style.backgroundColor = '#F2DEDE';popup.style.borderColor = '#EBCCD1';
-                break;
-            default:
-                popup.style.color = '#31708F';popup.style.backgroundColor = '#D9EDF7';popup.style.borderColor = '#BCE8F1';
-                break;
-
-        }
-
-        generateCloseButton();
-
-        popup.appendChild(closePopup);
+        setId(popup);
+        setPosition(popup, position);
+        applyMessageType(popup, type);
+        attachHideButton(popup);
 
         document.body.appendChild(popup);
 
-        //Destroy popup after setuped number of seconds
+        //Destroy popup after number of seconds
         setTimeout(function () {
-            document.getElementById(popup.id) ? document.body.removeChild(popup) : null;
+            removePopup(popup);
         }, duration * 1000);
-
     };
 
     /**
-     * Generates popup close button
+     * Appends closing button to popup window
+     *
+     * @param popup
      */
-    var generateCloseButton = function () {
-        closePopup.style.position = 'absolute';closePopup.style.top = '0';closePopup.style.right = '5px';closePopup.style.fontSize = '18px';closePopup.style.fontWeight = 'bold';closePopup.style.cursor = 'pointer';closePopup.className = 'close-popup';closePopup.innerHTML = '×';closePopup.title = 'Сообщение';
-        closePopup.addEventListener('click', function(){
-            document.getElementById(popup.id) ? document.body.removeChild(popup) : null;
-        })
+    var attachHideButton = function (popup) {
+        if (!popup.id) {
+            return;
+        }
+
+        var hideButton = fillAttributes(document.createElement('span'), elements.hideButton);
+
+        hideButton.addEventListener('click', function () {
+            removePopup(popup);
+        });
+
+        popup.appendChild(hideButton);
     };
 
     /**
-     * Checks if passed params are valid to generate popup
-     * @returns {boolean}
+     * Applies visual changes to popup based on message type
+     *
+     * @param popup
+     * @param messageType
      */
-    var validPopupDeclaration = function () {
-        return !(messageType == null || message == null);
+    var applyMessageType = function (popup, messageType) {
+        switch (messageType) {
+            case messageTypes.success:
+                fillAttributes(popup, {
+                    style: {
+                        color: '#3C763D',
+                        backgroundColor: '#DFF0D8',
+                        borderColor: '#D6E9C6'
+                    }
+                });
+                break;
+            case messageTypes.notice:
+                fillAttributes(popup, {
+                    style: {
+                        color: '#31708F',
+                        backgroundColor: '#D9EDF7',
+                        borderColor: '#BCE8F1'
+                    }
+                });
+                break;
+            case messageTypes.warning:
+                fillAttributes(popup, {
+                    style: {
+                        color: '#8A6D3B',
+                        backgroundColor: '#FCF8E3',
+                        borderColor: '#FAEBCC'
+                    }
+                });
+                break;
+            case messageTypes.error:
+                fillAttributes(popup, {
+                    style: {
+                        color: '#A94442',
+                        backgroundColor: '#F2DEDE',
+                        borderColor: '#EBCCD1'
+                    }
+                });
+                break;
+        }
     };
 
     /**
      * Gets unique identifier for popup.
-     * @returns {string}
+     *
+     * @param popup
      */
-    var getUniqueId = function () {
-        return 'lpn-message' + _this.popupId;
-    };
+    var setId = function (popup) {
 
+        this.popupId = typeof this.popupId == 'undefined' ? 1 : this.popupId + 1;
+
+        popup.id = 'lpn-message' + this.popupId;
+    };
 
     /**
      * Sets popup at position based on passed params. By default it will be placed in bottom left corner
+     *
+     * @param popup
+     * @param position
      */
-    var setPosition = function(){
+    var setPosition = function (popup, position) {
         var positionInfo = position.split(' ');
-        if(positionInfo.length == 2){
+        if (positionInfo.length == 2) {
             positionInfo[0] == 'top' ? popup.style.top = '20px' : popup.style.bottom = '20px';
             positionInfo[1] == 'right' ? popup.style.right = '20px' : popup.style.left = '20px';
         }
     };
 
+    /**
+     * Removes popup from DOM tree
+     *
+     * @param popup
+     */
+    var removePopup = function (popup) {
+        if (document.getElementById(popup.id)) {
+            document.body.removeChild(popup);
+        }
+    };
 
-    //Constructor
-    generatePopup();
+    /**
+     * Recursively declares DOM element attributes based on passed values
+     *
+     * @param targetElement
+     * @param attributes
+     * @returns {*}
+     */
+    var fillAttributes = function (targetElement, attributes) {
+        for (var i in attributes) {
+            if (attributes.hasOwnProperty(i)) {
+                if (typeof attributes[i] == "object") {
+                    fillAttributes(targetElement[i], attributes[i]);
+                } else {
+                    targetElement[i] = attributes[i];
+                }
+            }
+        }
+
+        return targetElement;
+    };
+
+    popupMessage(
+        message,
+        typeof messageType == 'undefined' || !messageType.toLowerCase() in messageTypes ? messageTypes.notice : messageType.toLowerCase(),
+        typeof position == 'undefined' || position.length == 0 ? 'bottom left' : position,
+        typeof duration == 'undefined' ? 5 : parseInt(duration)
+    );
 }
